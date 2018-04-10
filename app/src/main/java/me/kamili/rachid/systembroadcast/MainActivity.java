8e1a08b48a04d5d002174622aaaa9175017886e2
rachidKamili <rachidkamili.pro@gmail.com>
2018-04-09 20:07:52 -0400

    Receiving 5 different system broadcasts

--- app/src/main/java/me/kamili/rachid/systembroadcast/MainActivity.java
+++ app/src/main/java/me/kamili/rachid/systembroadcast/MainActivity.java
@@ -1,13 +1,40 @@
 package me.kamili.rachid.systembroadcast;
 
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import me.kamili.rachid.systembroadcast.receivers.MySystemReceiver;
+
 public class MainActivity extends AppCompatActivity {
+
+    private MySystemReceiver mySystemReceiver;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        mySystemReceiver = new MySystemReceiver();
+        IntentFilter intentFilter = new IntentFilter();
+        intentFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED);
+        intentFilter.addAction(Intent.ACTION_TIME_TICK);
+        intentFilter.addAction(Intent.ACTION_HEADSET_PLUG);
+        intentFilter.addAction(Intent.ACTION_POWER_CONNECTED);
+        intentFilter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        registerReceiver(mySystemReceiver, intentFilter);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        unregisterReceiver(mySystemReceiver);
     }
 }
---
+++ app/src/main/java/me/kamili/rachid/systembroadcast/receivers/MySystemReceiver.java
@@ -0,0 +1,47 @@
+package me.kamili.rachid.systembroadcast.receivers;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.widget.Toast;
+
+public class MySystemReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "MySystemReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+
+        String text;
+        switch (intent.getAction()) {
+            case Intent.ACTION_AIRPLANE_MODE_CHANGED:
+                boolean status = intent.getBooleanExtra("state", false);
+                text = "Airplane mode :" + status;
+                Log.d(TAG, "onReceive: " + text);
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+                break;
+            case Intent.ACTION_TIME_TICK:
+                text = "The current time has changed";
+                Log.d(TAG, "onReceive: " + text);
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+                break;
+            case Intent.ACTION_HEADSET_PLUG:
+                int state = intent.getIntExtra("state",0);
+                text = state==1? "Wired Headset plugged in" : "Wired Headset unplugged" ;
+                Log.d(TAG, "onReceive: " + text);
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+                break;
+            case Intent.ACTION_POWER_CONNECTED:
+                text = "Power got connected to the device";
+                Log.d(TAG, "onReceive: " + text);
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+                break;
+            case Intent.ACTION_POWER_DISCONNECTED:
+                text = "Power got disconnected to the device";
+                Log.d(TAG, "onReceive: " + text);
+                Toast.makeText(context, text, Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
